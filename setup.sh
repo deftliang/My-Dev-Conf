3f7c044f6427bf474ad485466dc08cb24348ff68
Deft <62738469+deftliang@users.noreply.github.com>
2022-01-13 10:45:28 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# hosts
+sudo cat ./hosts >> /etc/hosts
 
 # System-wise settings
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
