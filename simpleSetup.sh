28d013c4863fcfc73c0ff395beed5750ab4acba2
Deft <62738469+deftliang@users.noreply.github.com>
2022-11-07 23:47:52 +0800

    Update simpleSetup.sh

--- simpleSetup.sh
+++ simpleSetup.sh
@@ -5,6 +5,10 @@ sudo cat ./ubuntu-20.04LTS > /etc/aptsource.backup.list
 sudo apt update
 sudo apt install wget curl git openssh-server net-tools
 
+# install zsh
+sudo apt install -y zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 
 # install Nodejs
 curl -L https://raw.githubusercontent.com/tj/n/master/bin/n -o n
