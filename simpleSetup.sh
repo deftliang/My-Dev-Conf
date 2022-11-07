5dd8bb42308a9623d59b6a9ada8659119caa12e5
Deft <62738469+deftliang@users.noreply.github.com>
2022-11-07 23:45:59 +0800

    Create simpleSetup.sh

---
+++ simpleSetup.sh
@@ -0,0 +1,17 @@
+# my ubuntu config
+# basic
+sudo cp /etc/apt/source.list /etc/apt/source.backup.list
+sudo cat ./ubuntu-20.04LTS > /etc/aptsource.backup.list
+sudo apt update
+sudo apt install wget curl git openssh-server net-tools
+
+
+# install Nodejs
+curl -L https://raw.githubusercontent.com/tj/n/master/bin/n -o n
+sudo bash n lts
+mkdir ~/.npm-global
+npm config set prefix '~/.npm-global'
+npm install -g n
+
+
+#
