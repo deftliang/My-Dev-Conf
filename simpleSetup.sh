1b953fd7508847cf1073d8b646c313294fbc878d
Deft <62738469+deftliang@users.noreply.github.com>
2022-12-21 15:10:49 +0800

    Update simpleSetup.sh

--- simpleSetup.sh
+++ simpleSetup.sh
@@ -8,14 +8,3 @@ sudo apt install wget curl git openssh-server net-tools
 # install zsh
 sudo apt install -y zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-
-# install Nodejs
-curl -L https://raw.githubusercontent.com/tj/n/master/bin/n -o n
-sudo bash n lts
-mkdir ~/.npm-global
-npm config set prefix '~/.npm-global'
-npm install -g n
-
-
-#
